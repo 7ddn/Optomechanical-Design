1511eb43cd48d1eaf43b4fb7365d725224c4b7d9
ZhangHewei <hibikiprprpr@gmail.com>
2020-07-04 12:57:28 +0800

    lens status gui

--- Opt_Summer/Form1.Designer.cs
+++ Opt_Summer/Form1.Designer.cs
@@ -29,11 +29,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lensList = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.lensList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lensList
+            // 
+            this.lensList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lensList.Location = new System.Drawing.Point(12, 35);
+            this.lensList.Name = "lensList";
+            this.lensList.RowTemplate.Height = 27;
+            this.lensList.Size = new System.Drawing.Size(765, 218);
+            this.lensList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(14, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "光组信息：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "添加透镜";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(686, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "删除透镜";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lensList);
+            this.Name = "Form1";
+            this.Text = "Optic";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize) (this.lensList)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView lensList;
 
         #endregion
     }
--- Opt_Summer/Form1.cs
+++ Opt_Summer/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Opt_Summer
@@ -15,6 +8,82 @@ namespace Opt_Summer
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //lensList.DataSource = createSource();
+            lensList.ColumnCount = 4;
+            lensList.ColumnHeadersVisible = true;
+
+            lensList.Columns[0].Name = "表面类型";
+            lensList.Columns[1].Name = "曲率半径";
+            lensList.Columns[2].Name = "厚度";
+            lensList.Columns[3].Name = "折射率";
+
+            for (int i = 0; i < 3; i++)
+            {
+                int index = this.lensList.Rows.Add();
+                switch (i)
+                {
+                    case 0:
+                        this.lensList.Rows[index].Cells[0].Value = "物面";
+                        break;
+                    case 1:
+                        this.lensList.Rows[index].Cells[0].Value = "光阑";
+                        break;
+                    case 2:
+                        this.lensList.Rows[index].Cells[0].Value = "像面";
+                        break;
+                    default:
+                        break;
+                }
+
+                this.lensList.Rows[index].Cells[1].Value = "INFINITY";
+                this.lensList.Rows[index].Cells[2].Value = "0";
+                this.lensList.Rows[index].Cells[3].Value = "1";
+            }
+            
+            lensList.AllowUserToAddRows = false;
+            lensList.AllowUserToDeleteRows = false;
+            lensList.MultiSelect = false;
+            lensList.Columns[0].ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lensList.Rows.Insert(lensList.RowCount - 1, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var row = lensList.CurrentRow;
+            if (row == null) return;
+            if (row.Index == 1)
+            {
+                if (lensList.RowCount == 3)
+                {
+                    string message = "不能删除最后一个透镜";
+                    MessageBox.Show(message, "错误");
+                    return;
+                }
+                else
+                {
+                    lensList.Rows.Remove(row);
+                    lensList.Rows[1].Cells[0].Value = "光阑";
+                    return;
+                }
+            }
+            if (row.Index != 0 && row.Index != lensList.RowCount - 1)
+            {
+                lensList.Rows.Remove(row);
+            }
+            else
+            {
+                string message = "不能删除" + (row.Index == 0 ? "物面" : "像面");
+                MessageBox.Show(message,"错误");
+            }
+            
         }
     }
 }
